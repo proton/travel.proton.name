54be6d716d8e9d6bbe684a8822cef25f7c49cde0
Petr Savichev (proton) <psavichev@gmail.com>
2021-05-02 19:40:46 -0400

    sort points by country and city name

--- json_filler.rb
+++ json_filler.rb
@@ -11,7 +11,9 @@ tripster_files.each do |file_path|
   arr += JSON.parse(File.open(file_path).read)['cities']
 end
 
-arr.uniq! { |city| city['city_id'] }
+arr = arr
+  .uniq { |point| point['city_id'] }
+  .sort_by { |point| %w[country_en title_en].map { |k| point[k] } }
 
 File.open(cities_path, 'w') do |file|
   file.write(JSON.pretty_generate(arr))
