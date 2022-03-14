b0ce6b796e1a45ee5a89bc3e9daf30e9b4145edd
Peter Savichev <psavichev@gmail.com>
2022-03-14 07:04:06 -0500

    Tune popupAnchor

--- site/code.js
+++ site/code.js
@@ -39,7 +39,7 @@ const control = {
       // shadowSize:   [50, 64], // size of the shadow
       // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
       // shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      popupAnchor:  [0, -9] // point from which the popup should open relative to the iconAnchor
   });
   
   },
